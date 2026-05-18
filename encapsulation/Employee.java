a7d66a02b87b7f1a8cf1041c81e0b0b8007299aa
sahasthraa <sahasthrathummala@gmail.com>
2026-05-18 02:48:58 -0700

    employee details using encapsulation

---
+++ encapsulation/Employee.java
@@ -0,0 +1,21 @@
+
+
+public class Employee {
+    private String Name;
+    private double Salary;
+
+
+    public String getName(){
+        return Name;
+    }
+     public void setName(String Name){
+        this.Name = Name;
+     }
+    public double getSalary(){
+        return Salary;
+    }
+    public void setSalary(double Salary){
+        this.Salary = Salary;
+    }
+}
+
---
+++ encapsulation/Employee2.java
@@ -0,0 +1,14 @@
+
+public class Employee2 {
+public static void main(String args[]) {
+    Employee obj = new Employee();
+        
+      obj.setName("sahasthra");
+        System.out.println( "Employee Name: " + obj.getName());
+
+    
+
+    obj.setSalary(26000.00);
+System.out.println("Employee Salary: " + obj.getSalary());
+}    
+}
