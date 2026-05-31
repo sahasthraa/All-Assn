9da7a567f77f7f3b40c38884f6668f7f7dbdb4f6
sahasthraa <sahasthrathummala@gmail.com>
2026-05-31 07:19:29 -0700

    rollno sorting

---
+++ abstraaction/ArrayList/rollno.java
@@ -0,0 +1,20 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class rollno{
+
+    public static void main(String args[]){
+        ArrayList<Integer> rno=new ArrayList<>();
+        rno.add(21);
+        rno.add(22);
+        rno.add(23);
+        rno.add(24);
+        rno.add(25);
+        rno.add(5,26);
+        rno.set(2,22);
+       System.out.println(rno.size());
+
+        System.out.println("rollno:" +rno);
+        
+    }
+}
