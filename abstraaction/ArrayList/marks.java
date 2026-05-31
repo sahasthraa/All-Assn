2883301b7dd879c96115b8f23d21f64cb73285a8
sahasthraa <sahasthrathummala@gmail.com>
2026-05-31 07:03:11 -0700

    array sorting

---
+++ abstraaction/ArrayList/marks.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+public class marks {
+    public static void main(String args[]){
+        ArrayList<String> mks= new ArrayList<>();
+   mks.add("sahasthra " );
+   mks.add("vaishnavi");
+   mks.add("sathwika");
+   mks.add("meghna");
+   mks.add( 2,"laddu");
+   mks.set(4,"maggie");
+   
+   System.out.println("marks :" + mks);
+   System.out.println(mks.size());
+    }
+
+
+
+    
+}
